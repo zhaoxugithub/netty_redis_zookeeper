ddf0f7fab92fee68edb1788e77c319997bb21ce2
coder001 <coder001@qq.com>
2022-07-08 12:14:51 +0800

    优化代码

--- NettyDemos/src/main/java/com/crazymakercircle/netty/basic/NettyReactorTest.java
+++ NettyDemos/src/main/java/com/crazymakercircle/netty/basic/NettyReactorTest.java
@@ -4,12 +4,14 @@ import com.crazymakercircle.im.common.bean.User;
 import com.crazymakercircle.util.Logger;
 import com.crazymakercircle.util.ThreadUtil;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.EventExecutorChooserFactory;
 import org.junit.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class NettyReactorTest {
@@ -63,12 +65,13 @@ public class NettyReactorTest {
             }
         };
         for (int i = 0; i < 10; i++) {
-            pool.scheduleAtFixedRate(runTarget,1,1,TimeUnit.MINUTES);
+            pool.scheduleAtFixedRate(runTarget, 1, 1, TimeUnit.MINUTES);
 
         }
 
         ThreadUtil.sleepSeconds(1000);
     }
+
     @Test
     public void testNettyscheduleAtFixedRate() {
 
@@ -80,7 +83,7 @@ public class NettyReactorTest {
             }
         };
         for (int i = 0; i < 10; i++) {
-            ((ScheduledExecutorService) pool).scheduleAtFixedRate(runTarget,10,10,TimeUnit.SECONDS);
+            ((ScheduledExecutorService) pool).scheduleAtFixedRate(runTarget, 10, 10, TimeUnit.SECONDS);
 
         }
 
@@ -96,4 +99,201 @@ public class NettyReactorTest {
                 .build();
         System.out.println(user);
     }
+
+
+    static class IntegerWrapper {
+        Integer i = 0;
+        ReentrantLock lock;
+
+        public IntegerWrapper() {
+            lock = new ReentrantLock();
+        }
+
+        public Integer getValue() {
+            return i;
+        }
+
+
+    }
+
+    static class Task implements Runnable {
+        IntegerWrapper integerWrapper = null;
+        CountDownLatch latch;
+
+        public Task(IntegerWrapper i, CountDownLatch latch) {
+            this.integerWrapper = i;
+            this.latch = latch;
+
+        }
+
+        @Override
+        public void run() {
+            integerWrapper.lock.lock();
+            try {
+                integerWrapper.i++;
+            } finally {
+                integerWrapper.lock.unlock();
+            }
+
+            latch.countDown();
+        }
+
+        @Override
+        public String toString() {
+            return integerWrapper.toString();
+        }
+    }
+
+
+    @Test
+    public void testThreadPool() {
+
+        IntegerWrapper[] integerWrappers = new IntegerWrapper[100];
+
+        //500w次，8个线程 ，juc 程序运行时间： 19567ms
+        //500w次，8个线程 ，juc 程序运行时间： 19s
+        //1000w次，16个线程 ，juc 程序运行时间： 33101ms
+        //1000w次，16个线程 ，juc 程序运行时间： 33s
+
+//        Integer countPerInt = 50000;
+        Integer countPerInt = 100000;
+
+        CountDownLatch latch = new CountDownLatch(countPerInt * 100);
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 100; i++) {
+            integerWrappers[i] = new IntegerWrapper();
+        }
+
+//        ThreadPoolExecutor pool = ThreadUtil.getCpuIntenseTargetThreadPool();
+        ExecutorService pool = Executors.newFixedThreadPool(16);
+        for (int j = 0; j < countPerInt; j++) {
+            for (int i = 0; i < 100; i++) {
+                IntegerWrapper integerWrapper = integerWrappers[i];
+                pool.submit(new Task(integerWrapper, latch));
+
+            }
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+        System.out.println("程序运行时间： " + (endTime - startTime) / 1000 + "s");
+
+        for (int i = 0; i < 100; i++) {
+            IntegerWrapper integerWrapper = integerWrappers[i];
+            if (integerWrapper.i < countPerInt) {
+                System.err.printf(i + "  error:" + integerWrapper.getValue());
+            }
+        }
+    }
+
+
+    //对应的netty 的通道
+
+    static class IntegerWrapperNoneLock {
+        Integer i = 0;
+        EventExecutor eventloop;
+
+
+        public Integer getValue() {
+            return i;
+        }
+
+        public void regester(EventExecutor eventloop) {
+            this.eventloop=eventloop;
+
+        }
+    }
+
+    static class TaskNoneLock implements Runnable {
+        IntegerWrapperNoneLock integerWrapper = null;
+        CountDownLatch latch;
+
+        public TaskNoneLock(IntegerWrapperNoneLock i, CountDownLatch latch) {
+            this.integerWrapper = i;
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            integerWrapper.i++;
+            latch.countDown();
+        }
+
+        @Override
+        public String toString() {
+            return integerWrapper.toString();
+        }
+    }
+
+    @Test
+    public void testThreadPoolOfNetty() {
+
+        IntegerWrapperNoneLock[] integerWrappers = new IntegerWrapperNoneLock[100];
+
+        // 500次，8个线程 ，juc 程序运行时间： 19567ms
+        // 500次，8个线程 ，juc 程序运行时间： 19s
+        // 500次，8个线程 ，netty 程序运行时间： 16587ms
+        // 500次，8个线程 ，netty 程序运行时间： 16s
+
+        //1000w次，16个线程 ，juc 程序运行时间： 33101ms
+        //1000w次，16个线程 ，juc 程序运行时间： 33s
+        //
+        // 1000w次，16个线程 ，netty 程序运行时间：  29848ms
+        //1000w次，16个线程 ，netty 程序运行时间： 29s
+//        Integer countPerInt = 50000;
+        Integer countPerInt = 100000;
+
+        CountDownLatch latch = new CountDownLatch(countPerInt * 100);
+
+        long startTime = System.currentTimeMillis();
+
+        //创建netty  线程池
+        DefaultEventExecutorGroup pool = new DefaultEventExecutorGroup(16);
+//        ThreadPoolExecutor pool = ThreadUtil.getCpuIntenseTargetThreadPool();
+
+        for (int i = 0; i < 100; i++) {
+            integerWrappers[i] = new IntegerWrapperNoneLock();
+            EventExecutor eventloop = pool.next();
+            integerWrappers[i].regester(eventloop);
+
+        }
+
+
+        for (int j = 0; j < countPerInt; j++) {
+            for (int i = 0; i < 100; i++) {
+                IntegerWrapperNoneLock integerWrapper = integerWrappers[i];
+                EventExecutor eventloop = integerWrapper.eventloop;
+                eventloop.submit(new TaskNoneLock(integerWrapper, latch));
+
+            }
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+        System.out.println("程序运行时间： " + (endTime - startTime) / 1000 + "s");
+
+        for (int i = 0; i < 100; i++) {
+            IntegerWrapperNoneLock integerWrapper = integerWrappers[i];
+            if (integerWrapper.i < countPerInt) {
+                System.err.printf(i + "  error:" + integerWrapper.getValue());
+            }
+        }
+    }
+
+
 }
